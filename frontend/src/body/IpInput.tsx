047af0934b3282a5c17b1ac60ddefa0b3203f7a1
Alexander Laaser <alexlaaser@hotmail.de>
2024-02-01 11:58:09 +0100

    fixed ui display

--- frontend/src/body/IpInput.tsx
+++ frontend/src/body/IpInput.tsx
@@ -45,36 +45,36 @@ function IpInput() {
     <>
       <div className="flex w-full items-center justify-center font-montserrat">
         <div className="pt-14">
-          <div className="flex">
-            <input
-              id="ip_adress"
-              type="text"
-              placeholder="IP-Address"
-              className="text-sm sm:text-base relative border-t-2 border-l-2 border-b-2 border-r rounded-l-lg placeholder-gray-400 focus:border-orange-600 focus:outline-none py-2 pr-8 pl-8 border-zinc-950"
-              onChange={handleIpInput}
-            ></input>
-            <select
-              id="ip_size_input"
-              className=" outline-none border-t-2 border-r-2 border-b-2 border-l border-zinc-950 text-sm rounded-r-lg focus:border-orange-600 p-2.5 "
-            >
-              <option value="US">/21</option>
-              <option value="CA">/22</option>
-              <option value="FR">/23</option>
-              <option value="DE">/24</option>
-              <option value="DE">/25</option>
-              <option value="DE">/26</option>
-              <option value="DE">/27</option>
-            </select>
-            <div className=" flex pl-2 items-center justify-center">
-              <button className="inline-flex items-center justify-center w-6 h-6 mr-2 text-slate-50 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-orange-600">
-                <span className="text-2xl">+</span>
-              </button>
+          <div className="flex items-center justify-center">
+            <div className="mr-2">
+              <input
+                id="ip_adress"
+                type="text"
+                placeholder="Starting ip address"
+                defaultValue="10.0.0.0"
+                className="text-sm sm:text-base relative border rounded placeholder-gray-400 focus:border-orange-600 focus:outline-none pl-4 pr-20 border-zinc-950 h-10"
+                onChange={handleIpInput}
+              ></input>
+              <div className="text-blue-700 font-bold text-sm pt-2">
+                {/* {address_space} */}
+                255.255.255.255 - 255.255.255.255
+              </div>
             </div>
-          </div>
-          <div className="flex pt-4 justify-center items-center">
-            <div className="">IP-address space:</div>
-            <div className="pl-2 text-blue-700 font-bold text-xl">
-              {address_space}
+            <div className="">
+              <select
+                id="ip_size_input"
+                className=" text-sm sm:text-base outline-none border border-zinc-950 text-sm rounded focus:border-orange-600 pr-16 pl-4 h-10"
+              >
+                <option value="US">/21</option>
+                <option value="CA">/22</option>
+                <option value="FR">/23</option>
+                <option value="DE">/24</option>
+                <option value="DE">/25</option>
+                <option value="DE">/26</option>
+                <option value="DE">/27</option>
+              </select>
+
+              <div className="text-blue-700 font-bold text-sm pt-2">1024</div>
             </div>
           </div>
         </div>
