b131699598bb8f4c97211acf1a034f35d53b9ca8
alexlaaser <alexander.laaser@pexon-consulting.de>
2024-03-24 17:15:15 +0100

    changed acr

--- infrastructure/terraform/acr.tf
+++ infrastructure/terraform/acr.tf
@@ -1,17 +1,17 @@
-    # resource "azurerm_container_registry" "acr" {
-    #   name                = "acr"
-    #   resource_group_name = azurerm_resource_group.example.name
-    #   location            = azurerm_resource_group.example.location
-    #   sku                 = "Premium"
-    #   admin_enabled       = false
-    #   georeplications {
-    #     location                = "East US"
-    #     zone_redundancy_enabled = true
-    #     tags                    = {}
-    #   }
-    #   georeplications {
-    #     location                = "North Europe"
-    #     zone_redundancy_enabled = true
-    #     tags                    = {}
-    #   }
-    # }
+# resource "azurerm_container_registry" "acr" {
+#   name                = "acr"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
+#   sku                 = "Premium"
+#   admin_enabled       = false
+#   georeplications {
+#     location                = "East US"
+#     zone_redundancy_enabled = true
+#     tags                    = {}
+#   }
+#   georeplications {
+#     location                = "North Europe"
+#     zone_redundancy_enabled = true
+#     tags                    = {}
+#   }
+# }
