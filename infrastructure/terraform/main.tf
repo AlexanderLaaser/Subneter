c0c0b330ee56e86e52017fcef9e23fd27bd32336
alexlaaser <alexander.laaser@pexon-consulting.de>
2024-03-24 17:15:05 +0100

    added terraform backend

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,6 +1,15 @@
 provider "azurerm" {
   features {
-    
+
+  }
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = azurerm_resource_group.tfbackend.name
+    storage_account_name = azurerm_storage_account.tfbackend.name
+    container_name       = azurerm_storage_container.tfbackend.name
+    key                  = "${var.stage}.terraform.tfstate"
   }
 }
 
