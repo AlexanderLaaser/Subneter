fd066ff753b39e82d2f657a09bc612b85d12521f
alexlaaser <alexander.laaser@pexon-consulting.de>
2024-03-25 22:09:11 +0100

    created backend.tf

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -5,12 +5,3 @@ provider "azurerm" {
   use_oidc = true
 }
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-tfbackend"
-    storage_account_name = "sttfbackendsubneter"
-    container_name       = "terraform-state"
-    key                  = "dev.terraform.tfstate"
-  }
-}
-
