5c697b29377a33104bce0b7e98e7cf09b37f82ab
alexlaaser <alexander.laaser@pexon-consulting.de>
2024-03-25 23:00:55 +0100

    changed state key

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "rg-tfbackend"
     storage_account_name = "sttfbackendsubneter"
     container_name       = "terraform-state"
-    key                  = "dev.terraform.tfstate"
+    key                  = "terraform.tfstate"
   }
 }
