5a7d4fd5dd7edb81947a96fffd36b493c9a455b6
alexlaaser <alexander.laaser@pexon-consulting.de>
2024-04-05 20:26:44 +0200

    added role definition id

--- infrastructure/terraform/acr.tf
+++ infrastructure/terraform/acr.tf
@@ -8,6 +8,6 @@ resource "azurerm_container_registry" "acr" {
 
 resource "azurerm_role_assignment" "acrpushgithub" {
   scope              = azurerm_container_registry.acr.id
-  role_definition_id = "AcrPush"
+  role_definition_id = "/subscriptions/a50ff473-cc05-46ed-a7a6-8c5ed3c3907b/providers/Microsoft.Authorization/roleDefinitions/8311e382-0749-4cb8-b61a-304f252e45ec"
   principal_id       = var.clientid
 }
