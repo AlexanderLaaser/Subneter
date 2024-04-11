49d8d6f6666e14bd7c26506030f515d512c45db8
alexlaaser <alexander.laaser@pexon-consulting.de>
2024-04-11 11:35:24 +0200

    Standard_DS1_v2 added

--- infrastructure/terraform/aks.tf
+++ infrastructure/terraform/aks.tf
@@ -12,7 +12,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard B2ms"
+    vm_size    = "Standard_DS1_v2"
   }
 
   identity {
