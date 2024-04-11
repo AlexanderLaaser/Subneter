604b8314e816638ef686662ccc8086498ecbd914
alexlaaser <alexander.laaser@pexon-consulting.de>
2024-04-11 10:46:34 +0200

    changed vm sku & backend port

--- infrastructure/terraform/aks.tf
+++ infrastructure/terraform/aks.tf
@@ -12,7 +12,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_D2_v2"
+    vm_size    = "Standard B2ms"
   }
 
   identity {
