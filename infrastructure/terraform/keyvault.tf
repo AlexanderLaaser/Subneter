71570abc593d8407e0cf7ba190c4b55a33342c01
alexlaaser <alexander.laaser@pexon-consulting.de>
2024-04-13 00:13:11 +0200

    added object id for aks mi

--- infrastructure/terraform/keyvault.tf
+++ infrastructure/terraform/keyvault.tf
@@ -1,9 +1,5 @@
 data "azurerm_client_config" "current" {}
 
-data "azuread_service_principal" "sp_aks_mi" {
-  display_name = "azurekeyvaultsecretsprovider-aks-subneterdev"
-}
-
 resource "azurerm_key_vault" "subnetervault" {
   name                        = module.naming.key_vault.name
   location                    = azurerm_resource_group.main.location
@@ -18,7 +14,7 @@ resource "azurerm_key_vault" "subnetervault" {
 resource "azurerm_key_vault_access_policy" "access_policy_sp_aks_mi" {
   key_vault_id = azurerm_key_vault.subnetervault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azuread_service_principal.sp_aks_mi.object_id
+  object_id    = "ca962ecb-4ff2-4e75-8d80-9741378e307c"
 
   certificate_permissions = [
     "Get",
