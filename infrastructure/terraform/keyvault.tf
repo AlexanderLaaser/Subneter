26385dd53b940ecce07848ff20cb18b8bb9e3c5e
alexlaaser <alexander.laaser@pexon-consulting.de>
2024-04-13 00:07:41 +0200

    changed mi access to get

--- infrastructure/terraform/keyvault.tf
+++ infrastructure/terraform/keyvault.tf
@@ -1,5 +1,9 @@
 data "azurerm_client_config" "current" {}
 
+data "azuread_service_principal" "sp_aks_mi" {
+  display_name = "azurekeyvaultsecretsprovider-aks-subneterdev"
+}
+
 resource "azurerm_key_vault" "subnetervault" {
   name                        = module.naming.key_vault.name
   location                    = azurerm_resource_group.main.location
@@ -11,10 +15,10 @@ resource "azurerm_key_vault" "subnetervault" {
   sku_name                    = "standard"
 }
 
-resource "azurerm_key_vault_access_policy" "subneterlaaseraccess" {
+resource "azurerm_key_vault_access_policy" "access_policy_sp_aks_mi" {
   key_vault_id = azurerm_key_vault.subnetervault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = var.objectid
+  object_id    = data.azuread_service_principal_aks_mi.sp_aks_mi.object_id
 
   key_permissions = [
     "Get",
