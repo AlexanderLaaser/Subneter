1f230fc4b0bae1eb88052e01d63809d3135082d7
alexlaaser <alexander.laaser@pexon-consulting.de>
2024-04-28 22:49:41 +0200

    db local init = django connection

--- backend/subneter_project/subneter_project/settings.py
+++ backend/subneter_project/subneter_project/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,8 +88,12 @@ WSGI_APPLICATION = 'subneter_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
