a16903c5a592b6340ba951d19f9c12cb4a7200fb
alexlaaser <alexander.laaser@pexon-consulting.de>
2024-05-29 23:28:24 +0200

    added data sidebar

--- frontend/src/components/body/Body.tsx
+++ frontend/src/components/body/Body.tsx
@@ -1,8 +1,9 @@
 import Slogan from "./Slogan";
 import VnetInput from "./VnetInput";
 import Table from "./table/Table";
-import Sidebar from "./Sidebar";
+import HistorySidebar from "./HistorySidebar";
 import { useUserStore } from "../../store/UserStore";
+import UsageSidebar from "./UsageSidebar";
 
 function Body() {
   const { userLoginStatus } = useUserStore();
@@ -11,13 +12,17 @@ function Body() {
       {userLoginStatus ? null : <Slogan />}
 
       <div className="flex">
-        <div className="flex-1">{userLoginStatus ? <Sidebar /> : null}</div>
+        <div className="flex-1">
+          {userLoginStatus ? <HistorySidebar /> : null}
+        </div>
 
         <div className="">
           <VnetInput />
           <Table />
         </div>
-        <div className="flex-1"></div>
+        <div className="flex-1">
+          <UsageSidebar />
+        </div>
       </div>
     </div>
   );
